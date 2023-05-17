07e7525d39251e32af378bcd2877b43d315b2948
dmitriy <vdmitriy003@gmail.com>
2023-05-17 23:00:36 +0300

    refactor: dish resource

---
+++ app/Http/Resources/DishCategoryResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DishCategoryResource extends JsonResource
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+        ];
+    }
+}
